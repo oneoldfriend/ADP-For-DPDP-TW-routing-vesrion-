61b5c627a04d276a2a83bbc89bb5a23fd1d4c2b2
laoyiu <121375919@qq.com>
2019-07-26 16:07:06 +0800

    runtime info output

--- DLT/main.cpp
+++ DLT/main.cpp
@@ -6,6 +6,10 @@
 
 int main()
 {
+    clock_t start, end;
+    start = clock();
     Solver solver;
     solver.solve();
+    end = clock();
+    cout << "runtime: " << double(end - start) / CLOCKS_PER_SEC / 60.0 << " min" << endl;
 }
